0c4cbb68418d624cc557c5a7325135be5b9e5e2f
Kaye <sbonite21@gmail.com>
2021-09-25 23:56:38 +0800

    feat: add project 6

--- project6/express/models/programs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-
-const programSchema = new schema({
-	name: String,
-	active_flag: Boolean,
-});
-
-module.exports = mongoose.model('programs', programSchema);
